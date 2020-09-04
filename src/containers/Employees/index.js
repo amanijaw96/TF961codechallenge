19c7a9f87e568a9062aac23f3229686970dfc550
amani <amanijaw1996>
2020-09-04 10:01:12 +0300

    add skeleton on get employees

--- src/containers/Employees/index.js
+++ src/containers/Employees/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import EmployeesAPI from "./api";
 import { FiltersWrapper } from "./Employees_styled";
 import CustomSelect from "../../components/CustomSelect/CustomSelect";
+import Skeleton from "react-loading-skeleton";
+
 const page_size = 15;
 
 const Value = (name, value) => {
@@ -16,10 +18,13 @@ const Value = (name, value) => {
 export default function Employees({ ...props }) {
   const [employees, setEpmloyees] = React.useState([]);
   const [gender, setGender] = React.useState();
+  const [Loading, setLoading] = React.useState(false);
 
   const GetEmployees = (query) => {
+    setLoading(true);
     EmployeesAPI.GetEmployees(query)
       .then((res) => {
+        setLoading(false);
         setEpmloyees(res.data.results);
         console.log(res);
       })
@@ -55,25 +60,39 @@ export default function Employees({ ...props }) {
         ></CustomSelect>
       </FiltersWrapper>
 
-      <div className="row">
-        {employees.map((employee) => {
-          return (
-            <div className="col-lg-4 col-md-6">
-              <div className="card p-3 mt-3">
-                <img
-                  style={{ objectFit: "contain" }}
-                  src={employee.picture.large}
-                ></img>
-                <div className="mt-2">
-                  {Value("Name:", employee.name.first + employee.name.last)}
-                  {Value("Phone:", employee.phone)}
-                  {Value("Age:", employee.dob.age)}
+      {Loading ? (
+        <div className="row">
+          <div className="col-lg-4 col-md-6">
+            <Skeleton count={5} height={200} />
+          </div>
+          <div className="col-lg-4 col-md-6">
+            <Skeleton count={5} height={200} />
+          </div>
+          <div className="col-lg-4 col-md-6">
+            <Skeleton count={5} height={200} />
+          </div>
+        </div>
+      ) : (
+        <div className="row">
+          {employees.map((employee) => {
+            return (
+              <div className="col-lg-4 col-md-6">
+                <div className="card p-3 mt-3">
+                  <img
+                    style={{ objectFit: "contain" }}
+                    src={employee.picture.large}
+                  ></img>
+                  <div className="mt-2">
+                    {Value("Name:", employee.name.first + employee.name.last)}
+                    {Value("Phone:", employee.phone)}
+                    {Value("Age:", employee.dob.age)}
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
